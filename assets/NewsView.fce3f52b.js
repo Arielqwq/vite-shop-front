642f38147afbd0f8081374561368036bc1dc9e7f
Arielqwq <113668495+Arielqwq@users.noreply.github.com>
2023-02-21 02:25:11 +0000

    Deploying to gh-pages from @ Arielqwq/vite-shop-front@4ceb777f10d093d6a7fd9a88dfaad75482e156d5 🚀

---
+++ assets/NewsView.fce3f52b.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.b94e02d5.js";import{j as t,k as o,d as s,N as a,l as r,_ as n}from"./index.dabf6670.js";const c={id:"front-news"},_=r("h3",null,"front-news",-1);function d(l,f){return t(),o("div",c,[s(e,null,{default:a(()=>[_]),_:1})])}const i={};var h=n(i,[["render",d]]);export{h as default};
