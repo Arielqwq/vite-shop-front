35dff7e4103662bdbd532307bf3b6173328ed752
Ariel <dawei4102@gmail.com>
2023-03-01 21:30:52 +0800

    0301
    aos

--- src/pages/front/EventsView.vue
+++ src/pages/front/EventsView.vue
@@ -1,28 +1,38 @@
 <template lang="pug">
 #front-events(align="center")
   div.row
-    .cardinEventView.q-pa-lg.col-6(style=" margin-bottom:30px" v-for="event in events" :key="event._id" )
-      EventCard.eventCard(v-bind="event")
+    .cardinEventView.q-pa-lg.col-6( style=" margin-bottom:30px" v-for="event in events" :key="event._id" )
+      EventCard.eventCard(v-bind="event" data-aos="fade-up")
   </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, onMounted, nextTick } from 'vue'
 import { api } from '@/boot/axios'
 import Swal from 'sweetalert2'
 import EventCard from '@/components/EventCard.vue'
 
-const events = reactive([]);
+// aos
+import AOS from 'aos'
+// 或是改到config
+import 'aos/dist/aos.css'
 
-(async () => {
-  try {
-    const { data } = await api.get('/events')
-    events.push(...data.result)
-  } catch (error) {
-    Swal.fire({
-      icon: 'error',
-      title: '失敗',
-      text: error?.response?.data?.message || '發生錯誤'
-    })
-  }
-})()
+const events = reactive([])
+
+onMounted(() => {
+  (async () => {
+    try {
+      const { data } = await api.get('/events')
+      events.push(...data.result)
+      // aos
+      await nextTick()
+      AOS.init()
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: '失敗',
+        text: error?.response?.data?.message || '發生錯誤'
+      })
+    }
+  })()
+})
 </script>
--- src/pages/front/NewsView.vue
+++ src/pages/front/NewsView.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
 #front-news
   div.flex.justify-center.row
-    .cardinNewsView.flex.justify-center.col-4(style="margin-bottom:60px;" v-for="newscard in news" :key="newscard._id")
-      NewsCard.newscard(v-bind="newscard")
+    .cardinNewsView.flex.justify-center.col-4( style="margin-bottom:60px;" v-for="newscard in news" :key="newscard._id")
+      NewsCard.newscard(v-bind="newscard" data-aos="zoom-out-up")
 
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, nextTick } from 'vue'
 import { api } from '@/boot/axios'
 import Swal from 'sweetalert2'
 import NewsCard from '@/components/NewsCard.vue'
+
+// aos
+import AOS from 'aos'
+// 或是改到config
+import 'aos/dist/aos.css'
 
 const news = reactive([]);
 
@@ -18,6 +23,8 @@ const news = reactive([]);
   try {
     const { data } = await api.get('/news')
     news.push(...data.result)
+    await nextTick()
+    AOS.init()
   } catch (error) {
     Swal.fire({
       icon: 'error',
