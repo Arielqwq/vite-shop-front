ebd5b32b054b0c942374c0147c77a9bb3a5670a4
Arielqwq <113668495+Arielqwq@users.noreply.github.com>
2023-03-01 13:34:35 +0000

    Deploying to gh-pages from @ Arielqwq/vite-shop-front@194b345b91524a54917a7df26e8af49801373199 🚀

---
+++ assets/PaymentcardsView.1ac0b558.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.8952dab9.js";import{j as a,k as t,d as r,Y as s,l as c,_ as o}from"./index.f86514b6.js";const n={id:"Mypage-Paymentcards"},d=c("h3",null,"Mypage-Paymentcards",-1);function _(l,m){return a(),t("div",n,[r(e,null,{default:s(()=>[d]),_:1})])}const i={};var u=o(i,[["render",_]]);export{u as default};
