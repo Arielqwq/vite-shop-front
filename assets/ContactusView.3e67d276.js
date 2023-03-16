50b30fe0efc832295afa957ce0588a04f39f1fa1
Arielqwq <113668495+Arielqwq@users.noreply.github.com>
2023-03-16 15:32:18 +0000

    Deploying to gh-pages from @ Arielqwq/vite-shop-front@d65980554f505adcef56384444dfe484d31a7478 🚀

---
+++ assets/ContactusView.3e67d276.js
@@ -0,0 +1,1 @@
+import{Q as t}from"./QPage.d44dc12d.js";import{j as e,k as a,d as o,Y as s,l as c,_ as n}from"./index.182b0328.js";const r={id:"contactus"},_=c("h3",null,"front-Contactus",-1);function d(l,u){return e(),a("div",r,[o(t,null,{default:s(()=>[_]),_:1})])}const i={};var h=n(i,[["render",d]]);export{h as default};
